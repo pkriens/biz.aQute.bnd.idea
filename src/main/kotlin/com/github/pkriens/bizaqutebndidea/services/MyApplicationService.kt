035b4fd35808d210e672fc352ef8b8612ab28958
GitHub Action <action@github.com>
2021-11-16 09:39:10 +0000

    Template cleanup

--- src/main/kotlin/com/github/pkriens/bizaqutebndidea/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/pkriens/bizaqutebndidea/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.pkriens.bizaqutebndidea.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.pkriens.bizaqutebndidea.MyBundle
 
 class MyApplicationService {
 
